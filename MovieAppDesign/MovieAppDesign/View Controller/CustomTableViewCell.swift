1d00f1f018ae1832fc33a3be1c6b5fa69e201a64
DhakaLive <dhakalive@DhakaLives-MacBook-Pro-2.local>
2020-03-03 16:31:19 +0600

    Added image

---
+++ MovieAppDesign/MovieAppDesign/View Controller/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  MovieAppDesign
+//
+//  Created by DhakaLive on 3/3/20.
+//  Copyright © 2020 DhakaLive. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
